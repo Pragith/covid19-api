e52e0639d7cb6e880c6c28008dc9b8c811130f52
Pragith Prakash <pragith.prakash@cardinalpath.com>
2020-04-15 12:57:43 -0300

    Global - data refresh

--- examples/examples.py
+++ examples/examples.py
@@ -1,10 +1,28 @@
 #%%
 import requests, os, json
 import pandas as pd, numpy as np, plotly.express as px
+from urllib.request import urlopen
 
 # Functions
+'''
+# Input:
+api: Type of the API call
+args: The value for the requested API call
+
+# Output:
+Pandas DataFrame
+'''
 covid_api = lambda api, args: pd.read_csv(f'https://raw.githubusercontent.com/Pragith/covid19-api/master/api/{api}/{args}.csv')
 
+#%%
+# Ex - Eric - 1
+df = covid_api(api='date', args='2020-04-03')
+df.tail()
+
+# Ex - Eric - 2
+df = covid_api(api='country', args='canada/ontario')
+df.tail()
+##
 
 #%%
 ## Ex-1
@@ -57,6 +75,32 @@ fig.write_image(f'ex-3.png')
 covid_api(api='dimensions', args='country')
 covid_api(api='dimensions', args='state')
 covid_api(api='dimensions', args='countries_states')
+
+
+#%%
+## Ex-5
+# Map (WIP)
+# Get country == 'us'
+country = 'canada'
+df = covid_api(api='country', args=country)
+df.tail()
+#%%
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+fig = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
 
 
 #%%
